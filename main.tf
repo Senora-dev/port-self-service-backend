d5c3fc4f570653889a2ab0389f9ca3451906454a
Shaked Braimok Yosef <shaked.braimok.yosef@gmail.com>
2024-03-17 12:51:53 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ module "self_service_agent"{
 module "create_secret_runner"{
     source = "Senora-dev/self-service-runner/aws"
     version = "~>1.0.0"
-    action_identifier = "create-secrets"
+    action_identifier = "ACTION-IDENTIFIER"
     buildspec_file = templatefile("${path.module}/runners/sample.yaml", { port_client_id=module.self_service_agent.port_client_id_arn, port_client_secret=module.self_service_agent.port_client_secret_arn })
 }
